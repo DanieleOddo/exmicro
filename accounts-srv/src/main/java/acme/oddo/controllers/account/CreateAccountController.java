63ae4cd964a9b07644e65c783022383fbe934396
Daniele Oddo <daniele.oddo@outlook.com>
2022-10-29 19:06:50 +0200

    Update log with Lombok Annotation

--- accounts-srv/src/main/java/acme/oddo/controllers/account/CreateAccountController.java
+++ accounts-srv/src/main/java/acme/oddo/controllers/account/CreateAccountController.java
@@ -4,12 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class CreateAccountController {
 
     
     @GetMapping("/")
 	public @ResponseBody String greeting() {
+		log.info("Ciao Daniele");
 		return "Hello, World";
 	}
     
